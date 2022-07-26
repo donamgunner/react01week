f4dcf122403713abea7f2250aac643e855f052ed
donamgunner <dydwns9310@gmail.com>
2022-07-26 14:42:48 +0900

    주특기 react 1주차 개인과제 todo list 만들기

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+
+function Header () {
+    return (
+        <div className = "Header">
+            <div className ="maintitle">My Todo List</div>
+            <div className ="subtitle">React homework</div>
+        </div>
+    )
+}
+export default Header;
